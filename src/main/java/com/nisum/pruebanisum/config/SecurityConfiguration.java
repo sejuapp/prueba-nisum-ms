2bf54949eafb1b3c63cb5f06f61696adc3b87ccb
Ivan David Gonzalez <givandavid@gmail.com>
2023-02-27 22:50:41 -0500

    P2

--- src/main/java/com/nisum/pruebanisum/config/SecurityConfiguration.java
+++ src/main/java/com/nisum/pruebanisum/config/SecurityConfiguration.java
@@ -1,18 +1,35 @@
 package com.nisum.pruebanisum.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-public class SecurityConfiguration {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     protected static final String[] AUTH_WHITELIST = {
             "/**"
     };
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers(AUTH_WHITELIST);
+    public void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(AUTH_WHITELIST)
+                .permitAll().anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+
+        http.csrf().ignoringAntMatchers("/h2-console/**");
+        http.csrf().ignoringAntMatchers("/swagger-ui/**");
+        http.headers().frameOptions().sameOrigin();
+    }
+
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**").allowedOriginPatterns("*").allowedHeaders("*").allowCredentials(true)
+                .allowedMethods("GET", "PUT", "POST", "DELETE");
     }
 }
