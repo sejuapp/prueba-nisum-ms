03a8369d9b5815c11e7f90c99f6340742972b56c
Ivan David Gonzalez <givandavid@gmail.com>
2023-02-27 23:36:31 -0500

    .

--- src/main/java/com/nisum/pruebanisum/config/SecurityConfiguration.java
+++ src/main/java/com/nisum/pruebanisum/config/SecurityConfiguration.java
@@ -1,36 +1,6 @@
 package com.nisum.pruebanisum.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+public class SecurityConfiguration {
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
-
-    protected static final String[] AUTH_WHITELIST = {
-            "/**"
-    };
-
-
-    public void configure(final HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers(AUTH_WHITELIST)
-                .permitAll().anyRequest()
-                .authenticated()
-                .and()
-                .formLogin();
-
-        http.csrf().ignoringAntMatchers("/h2-console/**");
-        http.headers().frameOptions().sameOrigin();
-    }
-
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**").allowedOriginPatterns("*").allowedHeaders("*").allowCredentials(true)
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-    }
 
 }
